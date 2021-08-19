d84cebdb561ff76b24062e52a36c273258060f1f
3427439707@qq.com <xkl828583>
2021-08-19 16:02:21 +0800

    添加

---
+++ src/promise.js
@@ -0,0 +1,51 @@
+import { asyncMethods } from './method'
+
+function hasCallback(args) {
+  if (!args || typeof args !== 'object') return false
+
+  const callback = ['success', 'fail', 'complete']
+  for (const m of callback) {
+    if (typeof args[m] === 'function') return true
+  }
+  return false
+}
+
+function _promisify(func) {
+  if (typeof func !== 'function'){
+   return  Promise.resolve(func)
+  }
+  return (args = {}) =>
+    new Promise((resolve, reject) => {
+      func(
+        Object.assign(args, {
+          success: resolve,
+          fail: reject
+        })
+      )
+    })
+}
+
+export function promisifyAll(wx = {}, wxp = {}) {
+  Object.keys(wx).forEach(key => {
+    const fn = wx[key]
+    if (typeof fn === 'function' && asyncMethods.indexOf(key) >= 0) {
+      wxp[key] = args => {
+        if (hasCallback(args)) {
+          fn(args)
+        } else {
+          return _promisify(fn)(args)
+        }
+      }
+    } else {
+      wxp[key] = args=> new Promise((resolve, reject)=>{
+        try{
+          resolve(fn(args))
+        }catch(err){
+          reject(err)
+        }
+      })
+    }
+  })
+}
+
+export const promisify = _promisify
